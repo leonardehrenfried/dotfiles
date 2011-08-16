75bf63a3ea8dfedbe82ec99476a23cf5b0578445
Leonard Ehrenfried <leonard.ehrenfried@numberfour.eu>
2011-08-16 16:25:13 +0200

    more symlinks in install script

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #! /bin/bash -e
 
-# sets up the symlinks for all my dotfiles
-
-ln -s ~/.vimrc ./.vimrc
+# set up the symlinks for all dotfiles
+ln -s ~/.vimrc          ./.vimrc
+ln -s ~/.vim            ./.vim
+ln -s ~/.gitconfig      ./.gitconfig
