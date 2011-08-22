0db2f47beed96ab801f1892cac67390cc3288e70
Leonard Ehrenfried <leonard.ehrenfried@web.de>
2011-08-22 23:59:31 +0200

    submodule initialisation

--- install.sh
+++ install.sh
@@ -9,6 +9,11 @@ ln -s `pwd`/.profile        ~/.profile
 ln -s `pwd`/.ackrc          ~/.ackrc
 ln -s `pwd`/.gemrc          ~/.gemrc
 
+git submodule init 
+git submodule update
+git submodule foreach git pull
+
+
 cd .vim/bundle/command-t/ruby/command-t
 ruby extconf.rb
 make
