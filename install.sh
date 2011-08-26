d98240383af3e69518a0d67ef68fe6cba5139b8c
Leonard Ehrenfried <leonard.ehrenfried@numberfour.eu>
2011-08-26 19:33:22 +0200

    gvimdiff & bin in symlinks

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #! /bin/bash -e
 
 #create the symlinks for all dotfiles
-declare -a dotfiles=(.vimrc .vim .gitconfig .inputrc .profile .ackrc .gemrc .hgrc)
+declare -a dotfiles=(.vimrc .vim .gitconfig .inputrc .profile .ackrc .gemrc .hgrc bin)
 
 for i in "${dotfiles[@]}"
 do
